f9cb2a1fbffc84b86ae4d28b2de2a088fe4193df
robsonCarlosDosSantos <rcs.robson.carlos.santos@gmail.com>
2018-10-15 14:02:34 -0300

    create page contato

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ContactPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html',
 })
 export class ContactPage {
+
+  following = false;
+  user = {
+    name: 'Robson Carlos',
+    email: 'rcs.robson.carlos.santos@gmail.com',
+    profileImage: 'assets/imgs/avatar/robson.jpg',
+    coverImage: 'assets/imgs/background/background-5.jpg',
+    occupation: 'Analista de Sistemas',
+    location: 'Paulista - PE - Brazil',
+    description: 'Um homem sábio disse uma vez: quanto mais você fizer alguma coisa, melhor você se tornará nisso.',
+  };
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
