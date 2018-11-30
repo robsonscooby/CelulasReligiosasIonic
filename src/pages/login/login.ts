86413e620e250499c9714803223fc9d7d79fe158
robsonCarlosDosSantos <rcs.robson.carlos.santos@gmail.com>
2018-11-30 10:27:26 -0300

    ajuste para validar membro

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -48,12 +48,10 @@ export class LoginPage {
         this.navCtrl.setRoot('TabsPage');
       })
         .catch((error: any) => {
-          let toast = this.toastCtrl.create({ duration: 3000, position: 'bottom' });
           if (error.code == 'auth/invalid-email' || error.code == 'auth/user-disabled'
             || error.code == 'auth/user-not-found' || error.code == 'auth/wrong-password') {
-            toast.setMessage('E-mail ou senha inválida.');
+              this.msgToast('E-mail ou senha inválida.');
           }
-          toast.present();
         });
       await this.loadingService.dismiss();
     } catch (error) {
@@ -62,11 +60,22 @@ export class LoginPage {
     }
   }
 
+  msgToast(mgs: string): void {
+    let toast = this.toastCtrl.create({ duration: 3000, position: 'bottom' });
+    toast.setMessage(mgs);
+    toast.present();
+  } 
+
   buscarIgreja(email: string): Promise<Igreja> {
     return new Promise(async (resolve) => {
-      await this.igrejaService.getUser(email).subscribe(u => u.filter(r => {
-        resolve(this.retUser = r)
-      })).unsubscribe;
+      await this.igrejaService.getUser(email).subscribe((res) => {
+        if(!res.length){
+          this.msgToast('Membro não existe.');
+        }else{
+          this.retUser = res[0];
+        }
+        resolve();
+      });
     });
   }
 
